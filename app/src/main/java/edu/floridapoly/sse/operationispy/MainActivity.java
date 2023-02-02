ddcf51676bcbe4fa78b146cdc44f7b32bb49f69e
LaCo <lcooper9264@floridapoly.edu>
2023-02-02 12:55:33 -0500

    Adding empty Main Activity and XML for commit/push test

---
+++ app/src/main/java/edu/floridapoly/sse/operationispy/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.floridapoly.sse.operationispy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //testing to see if this file works
+}
