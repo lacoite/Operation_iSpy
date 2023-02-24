c40d32fdd185a3b78cab4158eb47bf2335383c17
LaCo <lcooper9264@floridapoly.edu>
2023-02-24 12:09:23 -0500

    Camera Test Prototype

--- app/src/main/java/edu/floridapoly/sse/operationispy/MainActivity.java
+++ app/src/main/java/edu/floridapoly/sse/operationispy/MainActivity.java
@@ -1,14 +1,76 @@
 package edu.floridapoly.sse.operationispy;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Button camButton;
+    Bitmap capturedImage;
+
+    private static final int ACTIVITY_REQUEST_CODE = 1000;
+    private static final int PERMISSION_REQUEST_CODE = 2000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        camButton = findViewById(R.id.camButton);
+
+        camButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkPermissionAndOpenCamera();
+            }
+        });
     }
-    //testing to see if this file works
+
+    private void checkPermissionAndOpenCamera() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CODE);
+            return;
+        }
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, ACTIVITY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode){
+            case ACTIVITY_REQUEST_CODE:
+                capturedImage = (Bitmap) data.getExtras().get("data");
+                imageView.setImageBitmap((capturedImage));
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode){
+            case PERMISSION_REQUEST_CODE:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    checkPermissionAndOpenCamera();
+                }
+                break;
+        }
+    }
 }
