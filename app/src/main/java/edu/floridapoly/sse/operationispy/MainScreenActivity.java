d3e0981954c8e6187ade36ca6d3a2dba82172031
LaCo <lcooper9264@floridapoly.edu>
2023-02-26 21:59:13 -0500

    Commented out MainActivity.java, this will no longer be the main activity and can later be deleted.

---
+++ app/src/main/java/edu/floridapoly/sse/operationispy/MainScreenActivity.java
@@ -0,0 +1,15 @@
+package edu.floridapoly.sse.operationispy;
+
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
