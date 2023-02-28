391090d08cf9f2efacc90b37471ab645641bb703
LaCo <lcooper9264@floridapoly.edu>
2023-02-28 13:06:15 -0500

    Prototyped Fragment button with Toast Message on click

--- app/src/main/java/edu/floridapoly/sse/operationispy/HomePromptFragment.java
+++ app/src/main/java/edu/floridapoly/sse/operationispy/HomePromptFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +20,9 @@ import android.view.ViewGroup;
  * IGNORE THIS CODE, I NEED TO REIMPLEMENT THE CAMERA!!! -L
  */
 public class HomePromptFragment extends Fragment {
+    View view;
+    Button targetIdentifiedButton;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -41,28 +46,36 @@ public class HomePromptFragment extends Fragment {
      * @return A new instance of fragment HomePromptFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static HomePromptFragment newInstance(String param1, String param2) {
-        HomePromptFragment fragment = new HomePromptFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+//    public static HomePromptFragment newInstance(String param1, String param2) {
+//        HomePromptFragment fragment = new HomePromptFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_prompt, container, false);
+        view = inflater.inflate(R.layout.fragment_home_prompt, container, false);
+        targetIdentifiedButton = view.findViewById(R.id.targetIdentifiedButton);
+        targetIdentifiedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"Fragment's Button", Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
     }
 }
